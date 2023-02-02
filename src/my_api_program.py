63c5232b7837b904909488c5aad8cb73346f915b
Teodor-Stelian Baltaretu <teodorstelian98@yahoo.com>
2023-02-02 20:48:14 +0200

    Added the masteries of all the champions

--- src/my_api_program.py
+++ src/my_api_program.py
@@ -1,8 +1,25 @@
 import requests
+import settings
 
-# Initialize
-API_key = 'RGAPI-93acbca8-5216-47ea-8334-786afd569b78'
-API_url = 'https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/TheStelinator'
+# Settings import
+api_key = settings.API
+summoner_name = settings.SUMMONER_NAME
+region = settings.REGION
 
-API_url = f'{API_url}?api_key={API_key}'
-print(requests.get(API_url))
+# Get the summoner's account ID
+summoner_url = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={api_key}"
+summoner_response = requests.get(summoner_url)
+summoner_data = summoner_response.json()
+account_id = summoner_data["id"]
+
+# Get the champion masteries for the player
+masteries_url = f"https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{account_id}?api_key={api_key}"
+masteries_response = requests.get(masteries_url)
+masteries_data = masteries_response.json()
+
+# Print the champion masteries for the player
+for mastery in masteries_data:
+    champion_id = mastery["championId"]
+    champion_level = mastery["championLevel"]
+    champion_points = mastery["championPoints"]
+    print(f"Champion ID: {champion_id} | Level: {champion_level} | Points: {champion_points}")
---
+++ src/settings.py
@@ -0,0 +1,3 @@
+API = "RGAPI-ecb2896b-de88-4cb8-80f0-f827ef0e83b4"
+SUMMONER_NAME = "TheStelinator"
+REGION = "eun1"
